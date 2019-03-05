074efc40e131bd8080931e911d72f3753051c0bf
Waga Odongo <wagaodongo@gmail.com>
2019-03-05 14:07:03 +0300

    solved the caching issue
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "306a84fe9a7453024560";
+/******/ 	var hotCurrentHash = "a47ecb2cc4281fbd9d5f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
