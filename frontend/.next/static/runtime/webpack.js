3056b3e0996a9d87886edcff05ffac474a3d6086
Waga Odongo <wagaodongo@gmail.com>
2019-03-05 13:44:26 +0300

    pagination
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "257443df8f5a2d4e29a0";
+/******/ 	var hotCurrentHash = "306a84fe9a7453024560";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
