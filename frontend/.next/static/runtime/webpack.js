04cf22c7702a220ea6baa6b964e9caaae5650377
Waga Odongo <wagaodongo@gmail.com>
2019-03-07 20:34:18 +0300

    connecting items to users
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d363bb3065c323ad46d5";
+/******/ 	var hotCurrentHash = "dad68a7163d9291589dc";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
