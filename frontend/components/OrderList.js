b14e7ba59b739073c39d4f6e1c41ce5d633bf85a
Waga Odongo <wagaodongo@gmail.com>
2019-03-13 19:50:16 +0300

    orderList
    Signed-off-by: Waga Odongo <wagaodongo@gmail.com>

---
+++ frontend/components/OrderList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import Link from 'next/link';
+import styled from 'styled-components';
+import {formatDistance} from 'date-fns';
+
+
+
+ class OrderList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default OrderList;
